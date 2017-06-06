cf8e102a7539a33da24bfaa08f86e861d0c6cb7b
Bozana Bokan <bozana.bokan@posteo.net>
2017-06-06 15:57:03 +0200

    install the DB schema with plugin installation

--- FundRefPlugin.inc.php
+++ FundRefPlugin.inc.php
@@ -26,28 +26,28 @@ class fundRefPlugin extends GenericPlugin {
     function getDescription() {
         return "Plugin for searching and saving funder name, funder id, and grant number";
     }
-	
+
     function register($category, $path) {
 		$success = parent::register($category, $path);
 		if ($success && $this->getEnabled()) {
-			
+
 			import('plugins.generic.fundRef.classes.FunderDAO');
 			$funderDao = new FunderDAO();
-			DAORegistry::registerDAO('FunderDAO', $funderDao);		
+			DAORegistry::registerDAO('FunderDAO', $funderDao);
 
 			HookRegistry::register('Templates::Submission::SubmissionMetadataForm::AdditionalMetadata', array($this, 'metadataFieldEdit'));
-			
+
 			HookRegistry::register('LoadComponentHandler', array($this, 'setupGridHandler'));
-			
+
 			HookRegistry::register('TemplateManager::display',array($this, 'addGridhandlerJs'));
-			
+
 			HookRegistry::register('Templates::Article::Details', array($this, 'addArticleDisplay'));
-				
+
         }
 		return $success;
 	}
-	
-	
+
+
 	/**
 	 * Permit requests to the Funder grid handler
 	 * @param $hookName string The name of the hook being invoked
@@ -55,27 +55,27 @@ class fundRefPlugin extends GenericPlugin {
 	 */
 	function setupGridHandler($hookName, $params) {
 		$component =& $params[0];
-		
+
 		if ($component == 'plugins.generic.fundRef.controllers.grid.FunderGridHandler') {
 			import($component);
 			FunderGridHandler::setPlugin($this);
 			return true;
 		}
 		return false;
-	}	
-	
-	
+	}
+
+
 	/**
 	 * Insert funder panel in the metadata form
 	 */
 	function metadataFieldEdit($hookName, $params) {
 		$smarty =& $params[1];
 		$output =& $params[2];
-		$request = $this->getRequest();		
+		$request = $this->getRequest();
 		$output .= $smarty->fetch($this->getTemplatePath() . 'metadataForm.tpl');
 		return false;
 	}
-	
+
 	/**
 	 * Add custom gridhandlerJS for backend
 	 */
@@ -89,10 +89,10 @@ class fundRefPlugin extends GenericPlugin {
 					'contexts' => 'backend',
 				)
 		);
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Hook to Templates::Article::Details and list funder information
 	 * @param $hookName string
@@ -101,37 +101,44 @@ class fundRefPlugin extends GenericPlugin {
 	function addArticleDisplay($hookName, $params) {
 		$templateMgr = $params[1];
 		$output =& $params[2];
-		
+
 		$article = $templateMgr->get_template_vars('article');
-		
+
 		$funderDao = DAORegistry::getDAO('FunderDAO');
 		$funders = $funderDao->getBySubmissionId($article->getId());
-		$funders = $funders->toArray();		
-		
+		$funders = $funders->toArray();
+
 		if ($funders){
 			$templateMgr->assign('funders', $funders);
 			$output .= $templateMgr->fetch($this->getTemplatePath() . 'listFunders.tpl');
 		}
-		
+
 		return false;
-		
-	}	
-	
-	
+
+	}
+
+
 	/**
 	 * @copydoc Plugin::getTemplatePath()
 	 */
 	function getTemplatePath($inCore = false) {
 		return parent::getTemplatePath($inCore) . 'templates/';
-	}	
-	
+	}
+
+	/**
+	 * @copydoc Plugin::getInstallSchemaFile()
+	 */
+	function getInstallSchemaFile() {
+		return $this->getPluginPath() . '/schema.xml';
+	}
+
 	/**
 	 * Get the JavaScript URL for this plugin.
 	 */
 	function getJavaScriptURL() {
 		return Request::getBaseUrl() . DIRECTORY_SEPARATOR . $this->getPluginPath() . DIRECTORY_SEPARATOR . 'js';
-	}	
-	
-   
+	}
+
+
 }
 ?>
